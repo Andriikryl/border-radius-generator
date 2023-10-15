042866964d1f8526a7cfe6f93f8261f930fa5abc
Andriikryl <andreyk557@gmail.com>
2023-10-15 21:29:57 +0300

    add input ragers

--- src/components/generator/Generator.tsx
+++ src/components/generator/Generator.tsx
@@ -2,55 +2,128 @@
 import React from "react";
 import { Container } from "../container/Container";
 import style from "./style.module.css";
+import { InputFrame } from "../inputFrame/InputFrame";
 
-function Range({ volume, onVolumeChange }) {
-  return (
-    <>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <label htmlFor="volume-slider">Audio volume:</label>
-        <input
-          type="range"
-          id="volume-slider"
-          min={0}
-          max={100}
-          value={volume}
-          onChange={(event) => {
-            onVolumeChange(event.target.value);
-          }}
-        />
-      </form>
+export default function Generator() {
+  const [generatorVolume, setGeneratorVolume] = React.useState(79);
+  const [generatorVolumeSecond, setGeneratorVolumeSecond] = React.useState(71);
+  const [generatorVolumeThree, setGeneratorVolumeThree] = React.useState(63);
+  const [generatorVolumeFour, setGeneratorVolumeFour] = React.useState(100);
+  const [generatorVolumeE, setGeneratorVolumeE] = React.useState(100);
+  const [generatorVolumeF, setGeneratorVolumeF] = React.useState(100);
+  const [generatorVolumeG, setGeneratorVolumeG] = React.useState(100);
+  const [generatorVolumeH, setGeneratorVolumeH] = React.useState(100);
 
-      <p>
-        <strong>Current value:</strong>
-        {volume}
-      </p>
-    </>
-  );
-}
-
-export default function Generator({ volume }) {
-  const [generatorVolume, setGeneratorVolume] = React.useState(volume);
-
-  const handleVolumeChange = (newVolume) => {
+  const handleVolumeChange = (newVolume: React.SetStateAction<number>) => {
     setGeneratorVolume(newVolume);
+  };
+  const handleVolumeChangeSecond = (
+    newVolume: React.SetStateAction<number>
+  ) => {
+    setGeneratorVolumeSecond(newVolume);
+  };
+
+  const handleVolumeChangeThree = (newVolume: React.SetStateAction<number>) => {
+    setGeneratorVolumeThree(newVolume);
+  };
+  const handleVolumeChangeFour = (newVolume: React.SetStateAction<number>) => {
+    setGeneratorVolumeFour(newVolume);
+  };
+  const handleVolumeChangeE = (newVolume: React.SetStateAction<number>) => {
+    setGeneratorVolumeE(newVolume);
+  };
+
+  const handleVolumeChangeF = (newVolume: React.SetStateAction<number>) => {
+    setGeneratorVolumeF(newVolume);
+  };
+  const handleVolumeChangeG = (newVolume: React.SetStateAction<number>) => {
+    setGeneratorVolumeG(newVolume);
+  };
+  const handleVolumeChangeH = (newVolume: React.SetStateAction<number>) => {
+    setGeneratorVolumeH(newVolume);
   };
 
   return (
     <section className={style.generator}>
       <Container>
-        <div className={style.main__box}>
-          <div
-            style={{
-              borderRadius: `${generatorVolume}% 71% 63% 100% / 39% 66% 100% 86%`,
-            }}
-            className={style.inner__box}
-          ></div>
+        <div className={style.flex__group}>
+          <div className={style.form__group}>
+            <InputFrame
+              value={generatorVolume}
+              set={setGeneratorVolume}
+              min={0}
+              max={100}
+              rangeLabel="a-range"
+              numberLabel="a-number"
+            ></InputFrame>
+            <InputFrame
+              value={generatorVolumeSecond}
+              set={setGeneratorVolumeSecond}
+              min={0}
+              max={100}
+              rangeLabel="b-range"
+              numberLabel="b-number"
+            ></InputFrame>
+            <InputFrame
+              value={generatorVolumeThree}
+              set={setGeneratorVolumeThree}
+              min={0}
+              max={100}
+              rangeLabel="c-range"
+              numberLabel="c-number"
+            ></InputFrame>
+            <InputFrame
+              value={generatorVolumeFour}
+              set={setGeneratorVolumeFour}
+              min={0}
+              max={100}
+              rangeLabel="d-range"
+              numberLabel="d-number"
+            ></InputFrame>
+          </div>
+          <div className={style.main__box}>
+            <div
+              style={{
+                borderRadius: `${generatorVolume}% ${generatorVolumeSecond}% ${generatorVolumeThree}% ${generatorVolumeFour}%  / ${generatorVolumeE}% ${generatorVolumeF}% ${generatorVolumeG}% ${generatorVolumeH}%`,
+              }}
+              className={style.inner__box}
+            ></div>
+          </div>
+          <div className={style.form__group}>
+            <InputFrame
+              value={generatorVolumeE}
+              set={setGeneratorVolumeE}
+              min={0}
+              max={100}
+              rangeLabel="e-range"
+              numberLabel="e-number"
+            ></InputFrame>
+            <InputFrame
+              value={generatorVolumeF}
+              set={setGeneratorVolumeF}
+              min={0}
+              max={100}
+              rangeLabel="f-range"
+              numberLabel="f-number"
+            ></InputFrame>
+            <InputFrame
+              value={generatorVolumeG}
+              set={setGeneratorVolumeG}
+              min={0}
+              max={100}
+              rangeLabel="g-range"
+              numberLabel="g-number"
+            ></InputFrame>
+            <InputFrame
+              value={generatorVolumeH}
+              set={setGeneratorVolumeH}
+              min={0}
+              max={100}
+              rangeLabel="h-range"
+              numberLabel="h-number"
+            ></InputFrame>
+          </div>
         </div>
-        <Range volume={generatorVolume} onVolumeChange={handleVolumeChange} />
       </Container>
     </section>
   );
---
+++ src/components/inputFrame/InputFrame.tsx
@@ -0,0 +1,55 @@
+import { useId } from "react";
+import style from "./style.module.css";
+interface InputProps {
+  rangeLabel: string;
+  numberLabel: string;
+  value: number;
+  set: (newValue: number) => void;
+  min?: number;
+  max?: number;
+}
+
+export function InputFrame({
+  value,
+  rangeLabel,
+  numberLabel,
+  set,
+  min = -500,
+  max = 500,
+}: InputProps) {
+  const id = useId();
+  const rangeValumeId = `${id}-range-value`;
+  const numberValumeId = `${id}-number-value`;
+  return (
+    <form className={style.form}>
+      <div className={style.label__box}>
+        <label className={style.label} htmlFor={rangeValumeId}>
+          {rangeLabel}
+        </label>
+        <input
+          className={style.input}
+          value={value}
+          id={rangeValumeId}
+          type="range"
+          min={min}
+          max={max}
+          onChange={(e) => set(parseFloat(e.target.value))}
+        />
+      </div>
+      <div className={style.label__box}>
+        <label className={style.label} htmlFor={numberValumeId}>
+          {numberLabel}
+        </label>
+        <input
+          className={style.input}
+          type="number"
+          id={numberValumeId}
+          value={value}
+          min={min}
+          max={max}
+          onChange={(e) => set(parseFloat(e.target.value) || 0)}
+        />
+      </div>
+    </form>
+  );
+}
